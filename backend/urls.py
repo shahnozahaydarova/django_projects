dca5c406263d8699bf718302d94cec0c8b503631
shahnozahaydarova <shahnozahaydarova03@gmail.com>
2022-12-29 22:32:07 +0500

    online

--- backend/urls.py
+++ backend/urls.py
@@ -3,4 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', home),
+    path('404', notfile, name="404"),
+    path('about', about,name="about"),
+    path('adlist',ad_list,name="adlist")
 ]
--- backend/views.py
+++ backend/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'index.html')
+
+def notfile(request):
+    return render(request,'404.html')
+def about(request):
+    return render(request,'about-us.html')
+
+def ad_list(request):
+    return render(request,'ad-list-view.html')
