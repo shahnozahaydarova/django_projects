77ac79903556f41f2140c3cbc775724ddd08bd2f
shahnozahaydarova <shahnozahaydarova03@gmail.com>
2022-12-30 21:44:28 +0500

    online

--- backend/urls.py
+++ backend/urls.py
@@ -3,7 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('', home),
-    path('404', notfile, name="404"),
-    path('about', about,name="about"),
-    path('adlist',ad_list,name="adlist")
+    path('404', notfile, name="404.html"),
+    path('about', about,name="about.html"),
+    path('adlist',ad_list,name="adlist.html"),
+    path('adlisting',ad_listing,name="adlisting.html"),
+    path('blog',blog,name="blog.html"),
+    path('category',category,name="category.html"),
+    path('base',contact,name='contact.html'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -5,8 +5,21 @@ def home(request):
 
 def notfile(request):
     return render(request,'404.html')
+
 def about(request):
     return render(request,'about-us.html')
 
 def ad_list(request):
     return render(request,'ad-list-view.html')
+
+def ad_listing(request):
+    return render(request,'ad-listing.html')
+
+def blog(request):
+    return render(request,'blog.html')
+
+def category(request):
+    return render(request,'category.html')
+
+def contact(request):
+    return render(request,'contact.html')
