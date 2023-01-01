2d257c874967790761ee9aeef92351f1d13143df
shahnozahaydarova <shahnozahaydarova03@gmail.com>
2023-01-01 21:18:10 +0500

    online

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home),
+    path('', home,name="index"),
     path('404', notfile, name="404"),
     path('about', about,name="about"),
     path('adlist',ad_list,name="adlist.html"),
